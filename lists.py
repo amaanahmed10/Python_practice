c849036da693a9400bb84d88bb7c7edeeec4ed22
amaanahmed10 <amaanahmed2030@gmail.com>
2024-02-02 19:30:51 +0530

    Commit

---
+++ lists.py
@@ -0,0 +1,14 @@
+# #These are mutable i.e. their values can be changed with .append and so on
+lists = ['size', 10, 20, 30]
+
+new_model = 50
+
+lists.append(new_model)
+
+lists.pop(2)
+
+lists[1] = 15
+
+print(lists[2:])
+
+
---
+++ tuples.py
@@ -0,0 +1,5 @@
+specs = ("size", 'rotors', 'payload')
+
+print(specs)
+
+#These are immutable i.e. their values cannot be changed
