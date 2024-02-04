9b3d9b3c54bb6f12261bd2aeaacabfb8838a1e09
amaanahmed10 <amaanahmed2030@gmail.com>
2024-02-05 02:28:04 +0530

    5/2/2024 Made the image available in project folder also by changing image path.

--- OpenCV/read image.py
+++ OpenCV/read image.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-morning_wallpaper = cv.imread (r"\Users\aa882\Pictures\Wallpapers\1.jpg")
+morning_wallpaper = cv.imread ("/Users/aa882/PycharmProjects/pythonProject/OpenCV/1.jpg")
 
 cv.imshow( 'Test', morning_wallpaper)
 
